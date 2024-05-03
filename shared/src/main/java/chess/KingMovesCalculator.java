031565a81667e5f7f9d8edb38c381e901a7a501d
joshuamontierth <joshuamontierth@gmail.com>
2024-05-02 21:12:46 -0600

    Added Queen Movement

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -5,7 +5,8 @@ import java.util.Collection;
 public class KingMovesCalculator extends PieceMovesCalculator {
     @Override
     Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
-
+        if()
     }
+
 }
 
--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,10 +1,35 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class QueenMovesCalculator extends PieceMovesCalculator {
     @Override
     Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> out = new ArrayList<ChessMove>();
+        moveHelper(board, position, 1,1,out);
+        moveHelper(board, position, 1,-1,out);
+        moveHelper(board, position, -1,1,out);
+        moveHelper(board, position, -1,-1,out);
+        moveHelper(board, position, 1,0,out);
+        moveHelper(board, position, 0,1,out);
+        moveHelper(board, position, 0,-1,out);
+        moveHelper(board, position, -1,0,out);
+        return out;
+    }
 
+    void moveHelper(ChessBoard board, ChessPosition position, int x, int y, Collection<ChessMove> out) {
+        ChessPosition currentPos = position;
+        while(currentPos.getRow() <= 8 && currentPos.getColumn() <= 8 && currentPos.getRow() >= 1 && currentPos.getColumn() >= 1) {
+            if (board.getPiece(currentPos) != null) {
+                if (board.getPiece(currentPos).pieceColor != board.getPiece(position).pieceColor) {
+                    out.add(new ChessMove(position, currentPos, null));
+                }
+
+            } else {
+                currentPos.rowPos += x;
+                currentPos.colPos += y;
+            }
+        }
     }
 }
