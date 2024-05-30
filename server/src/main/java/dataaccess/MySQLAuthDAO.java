518f4e4faed067772d3e8bab6a8dae950acf7e81
joshuamontierth <joshuamontierth@gmail.com>
2024-05-30 17:51:44 -0600

    First JDMS method

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -2,10 +2,23 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MySQLAuthDAO implements AuthDAOInterface{
     @Override
-    public void createAuth(AuthData auth) {
+    public void createAuth(AuthData auth)  {
+        try (Connection c = DatabaseManager.getConnection()) {
+            try (var preparedStatement = c.prepareStatement("INSERT INTO auth (authToken, username) VALUES (?, ?)")) {
+                ResultSet rs = preparedStatement.executeQuery();
+                rs.next();
+                System.out.println(rs.getInt(1));
+            }
+        }
+        catch (Exception e){
 
+        }
     }
 
     @Override
