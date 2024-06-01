614d1d5acfb8c9d9c44fe24609231900745cb7c8
joshuamontierth <joshuamontierth@gmail.com>
2024-06-01 12:00:21 -0600

    Finished MySQLAuthDAO implementation

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -11,28 +11,59 @@ public class MySQLAuthDAO implements AuthDAOInterface{
     public void createAuth(AuthData auth)  {
         try (Connection c = DatabaseManager.getConnection()) {
             try (var preparedStatement = c.prepareStatement("INSERT INTO auth (authToken, username) VALUES (?, ?)")) {
-                ResultSet rs = preparedStatement.executeQuery();
-                rs.next();
-                System.out.println(rs.getInt(1));
+                preparedStatement.setString(1,auth.authToken());
+                preparedStatement.setString(2,auth.username());
+                preparedStatement.executeUpdate();
             }
         }
         catch (Exception e){
-
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        try (Connection c = DatabaseManager.getConnection()) {
+            try (var preparedStatement = c.prepareStatement("SELECT authToken, username FROM auth WHERE authToken = ?")) {
+                preparedStatement.setString(1,authToken);
+                var rs = preparedStatement.executeQuery();
+                if (rs.next()) {
+                    return new AuthData(rs.getString("authToken"),rs.getString("username"));
+                }
+                else {
+                    throw new DataAccessException("Error: unauthorized");
+                }
+            }
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
+        try (Connection c = DatabaseManager.getConnection()) {
+            try (var preparedStatement = c.prepareStatement("DELETE FROM auth WHERE authToken = ?")) {
+                preparedStatement.setString(1,authToken);
+                preparedStatement.executeQuery();
+            }
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void clear() {
+        try (Connection c = DatabaseManager.getConnection()) {
+            try (var preparedStatement = c.prepareStatement("DROP TABLE auth")) {
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
 
     }
 }
