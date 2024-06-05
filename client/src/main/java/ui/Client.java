f3fdfd2d86ffc59a010e74e1575d10189215891c
joshuamontierth <joshuamontierth@gmail.com>
2024-06-04 23:58:59 -0600

    Fixed loops in UI

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -9,6 +9,7 @@ import static java.lang.System.exit;
 
 public class Client {
     private boolean firstPreLogin = true;
+    private boolean firstPostLogin = true;
     private String authToken = null;
     private ServerFacade server;
 
@@ -17,8 +18,12 @@ public class Client {
         server = new ServerFacade(hostname, port);
         System.out.println("Welcome to 240 Chess");
 
-        while(authToken == null) {
+        while(true) {
             prelogin();
+            while(authToken != null) {
+                postLogin();
+            }
+
         }
     }
     private void printPreloginOptions() {
@@ -36,6 +41,7 @@ public class Client {
         System.out.println("4. Observe Game");
         System.out.println("5. Logout");
         System.out.println("6. Help");
+        System.out.println("Please select an option");
 
     }
 
@@ -49,16 +55,20 @@ public class Client {
             int option = scanner.nextInt();
             switch (option) {
                 case 1:
-                    RegisterUser();
+                    registerUser();
+                    System.out.println("User successfully registered");
                     break;
                 case 2:
-                    LoginUser();
+                    loginUser();
+                    System.out.println("Login successful");
                     break;
                 case 3:
                     exit(0);
                 case 4:
                     printPreloginOptions();
                     break;
+                case 5:
+                    logout();
                 default:
                     System.out.println("Invalid option, please enter a number between 1 and 4");
 
@@ -68,14 +78,65 @@ public class Client {
             System.out.println("Invalid input, please enter a number between 1 and 4");
         }
 
-
     }
-
     private void postLogin() {
+        if (firstPostLogin) {
+            printPostloginOptions();
+            firstPostLogin = false;
+        }
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    createGame();
+                    System.out.println("Game created");
+                    break;
+                case 2:
+                    listGames();
+                    break;
+                case 3:
+                    joinGame(false);
+                    System.out.println("Game joined");
+                    break;
+                case 4:
+                    joinGame(true);
+                    System.out.println("Game joined as observer");
+                    break;
+                case 5:
+                    logout();
+                    firstPostLogin = true;
+                    firstPreLogin = true;
+                    authToken = null;
+                    System.out.println("Logged out successfully\n");
+                    break;
+                case 6:
+                    printPostloginOptions();
+                    break;
+                default:
+                    System.out.println("Invalid option, please enter a number between 1 and 6");
+
+            }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input, please enter a number between 1 and 6");
+        }
 
     }
 
-    private void LoginUser() {
+    private void listGames() {
+    }
+
+    private void createGame() {
+    }
+    private void joinGame(boolean observerMode) {
+    }
+    private void logout() {
+    }
+
+
+
+    private void loginUser() {
         System.out.println("Please enter your username:");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
@@ -89,7 +150,7 @@ public class Client {
         }
 
     }
-    private void RegisterUser() {
+    private void registerUser() {
         System.out.println("Please enter your username:");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
