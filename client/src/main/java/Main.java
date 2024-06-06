c2e3cc4670ba2ee7901ec0b89c4cdd0230040070
joshuamontierth <joshuamontierth@gmail.com>
2024-06-06 16:47:18 -0600

    Fixed modules pt 2

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,12 +1,10 @@
 
-import server.*;
+
 import ui.Client;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("♕ 240 Chess Client: ");
-        Server server = new Server();
-        server.run(5000);
         Client client = new Client();
         client.run("localhost",5000);
 
